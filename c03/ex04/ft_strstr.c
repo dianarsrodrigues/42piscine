5fc701d2eef23576c41863357e6c267f5865c495
Diana Rodrigues <dianarodrigues@Air-de-Diana.lan>
2022-07-25 23:32:09 +0100

    c03v2

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -16,5 +16,32 @@
 
 char	*ft_strstr(char *str, char *to_find)
 {
+    int i;
+	int j;
 
+	i = 0;
+	if (to_find[j] == '\0')
+        return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+int main (void) 
+{
+    char str[] = "Diana Rodrigues 42 Porto";
+    char to_find[] = "42 Porto";
+    char *text;
+    
+    text = ft_strstr(str, to_find);
+    printf("The substring is: %s\n", text);
+    return(0);
 }
---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+    
+}
