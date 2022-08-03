aaf0829b53bea76475b4a9df6f8c6d3e89c70b79
Diana Rodrigues <dianarodrigues@MacBook-Air-de-Diana.local>
2022-08-03 23:43:04 +0100

    panicmode

--- treinoexame/exame03/01/repeat_alpha.c
+++ treinoexame/exame03/01/repeat_alpha.c
@@ -15,20 +15,27 @@
 int	main(int argc, char **argv)
 {
 	int	i;
-	int	j;
 	int a;
 	
 	i = 0;
-	a = 'a';
-	j = 1;
+	a = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i] != '\0')
 		{
-			j = (argv[1][i] - a); 
-			write (1, &argv[1][i], 1);
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				a = argv[1][i] - 64;
+			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				a = argv[1][i] - 96;
+			while(a)
+			{
+				write (1, &argv[1][i], 1);
+				a--;
+			}
+			a = 1;
 			i++;
 		}
 	}
+	write(1, "\n", 1);
 	return (0);
 }
